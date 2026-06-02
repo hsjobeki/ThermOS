67edb52c03885bdb21ca0a4c22fe702c1a4c40a7
Johannes Kirschbauer <hsjobeki@gmail.com>
2026-06-02 15:48:04 +0200

    system/qemu: configure networkd

--- systems/qemu.nix
+++ systems/qemu.nix
@@ -4,6 +4,13 @@ import ../default.nix {
       # Password: thermos
       rootHashedPassword = "$6$thermos$H0ll22GovTVsmgXyGSxBB1rAwU.QF6D/nFspidCXj0vFJ6YzUUzhs1r8/mEiXnb0IUUP8t2tChAmwA.vEXH9G/";
     };
+    # Static IP on QEMU user-mode (SLIRP) net. DHCP needs AF_PACKET, which the
+    # kernel lacks (CONFIG_PACKET=m, unloaded), so DHCP cannot work yet.
+    "/services/networkd" = {
+      enable = true;
+      useDHCP = false;
+      addresses = [ "10.0.2.15/24" ];
+    };
     "/services/getty" = {
       ttys = [ ];
       serialTtys = [ "ttyS0" ];
